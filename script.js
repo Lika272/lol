12645411cbb5329c657512617728400ee7db72ac
Lika Khabelov <74554786+Lika272@users.noreply.github.com>
2021-01-14 18:13:12 +0400

    lol
    lol

---
+++ script.js
@@ -0,0 +1,31 @@
+/*let a=12
+let b=54
+let reverse =a;
+a=b;
+b=reverse;
+console.log(a, 
+    b,)*/
+    /*
+    let sum = 0;
+    for(i=1; i <= 100 ; i++){
+if( i % 2 == 0){
+sum += i;
+}
+    }
+    console.log(sum)
+    */
+ /* for(i=1; i <= 100 ; i++){
+    if(i % 5 == 0 && i % 3 == 0){
+console.log("FizzBuzz")
+       }
+else if( i % 3 == 0){
+console.log("Fizz")
+}
+else if( i % 5 == 0){
+console.log("Buzz")}
+else{
+    console.log(i)
+}
+    }
+*/
+
